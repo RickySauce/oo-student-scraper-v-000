a814e4e28f60ea8c79fec252973b749596da775c
Eric DeSimone <eric.desimone@cortland.edu>
2018-07-04 15:09:41 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,6 @@ require 'nokogiri'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    binding.pry
     index_page = Nokogiri::HTML(File.read('./fixtures/student-site/index.html'))
     students = []
     index_page.css("div.student-card").each do |student_info|
